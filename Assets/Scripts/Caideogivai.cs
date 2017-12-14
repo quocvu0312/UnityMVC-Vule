dd15272711419004b0d02d2c891576b8c9bb3b95
vuledev0312 <vuledev0312@gmail.com>
2017-12-14 14:51:06 +0700

    update caideogivai.cs conflig

--- Assets/Scripts/Caideogivai.cs
+++ Assets/Scripts/Caideogivai.cs
@@ -5,15 +5,15 @@ using UnityEngine;
 public class Caideogivai : MonoBehaviour {
 
 	// Use this for initialization
-<<<<<<< HEAD
+
     
 	void Start () {
 
      
-=======
-	void Start () {
+
+	
 		
->>>>>>> b376dd90c530c9480f1fab6a3e0c8ac74f9f2eec
+
 	}
 	
 	// Update is called once per frame
