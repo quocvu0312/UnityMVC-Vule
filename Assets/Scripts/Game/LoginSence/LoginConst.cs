e490e5986819d01b25b269ffeeb72cfdc73f3c91
vuledev0312 <vuledev0312@gmail.com>
2017-12-14 11:08:15 +0700

    first update source

---
+++ Assets/Scripts/Game/LoginSence/LoginConst.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoginConst  {
+
+
+    public  const string LoginView = "loginview";
+}
---
+++ Assets/Scripts/Game/LoginSence/LoginSenceView.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoginSenceView : BaseWindow {
+
+    public override void InitWidget()
+    {
+       //Debug.Log
+
+        Debug.Log("cai loz");
+    }
+
+    public override void Awake()
+    {
+        base.Awake();
+
+    }
+
+    public override void AddEventListener()
+    {
+        
+        MVCEvent.Instancle.addEventListener(LoginConst.LoginView,loginview);
+        MVCEvent.Instancle.addEventListener(LoginConst.LoginView, (vl) =>
+        {
+            Hide();
+        });
+    }
+    public override void RemoveEventListener()
+    {
+        //throw new System.NotImplementedException();
+    }
+
+    public void loginview(object data)
+    {
+        Debug.Log(data);
+    }
+}
